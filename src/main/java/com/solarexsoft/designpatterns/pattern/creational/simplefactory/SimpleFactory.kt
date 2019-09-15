34da34ddfaf0ead2123bae23455a33196e6b9833
Solarex <rh.hou.work@gmail.com>
2019-09-15 22:48:57 +0800

    simple factory

---
+++ src/main/java/com/solarexsoft/designpatterns/pattern/creational/simplefactory/SimpleFactory.kt
@@ -0,0 +1,31 @@
+package com.solarexsoft.designpatterns.pattern.creational.simplefactory
+
+/**
+ * Created by houruhou on 2019/9/15.
+ * Desc:
+ */
+interface Video {
+    fun produce()
+}
+
+class JavaVideo : Video {
+    override fun produce() {
+        println("produce java video")
+    }
+}
+
+class PythonVideo : Video {
+    override fun produce() {
+        println("produce python video")
+    }
+}
+
+class VideoFactory {
+    companion object {
+        fun <T:Video> createVideo(clz: Class<T>):T = clz.newInstance()
+    }
+}
+
+fun main() {
+    VideoFactory.createVideo(JavaVideo::class.java).produce()
+}
