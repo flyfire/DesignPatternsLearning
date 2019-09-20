dbb896bbba9c12e9b86740af0627d4338d2d4090
Solarex <rh.hou.work@gmail.com>
2019-09-20 09:24:11 +0800

    templatemethod

---
+++ src/main/java/com/solarexsoft/designpatterns/pattern/behavioral/templatemethod/TemplateMethod.kt
@@ -0,0 +1,57 @@
+package com.solarexsoft.designpatterns.pattern.behavioral.templatemethod
+
+/**
+ * Created by houruhou on 2019/9/19.
+ * Desc:
+ */
+abstract class ACourse {
+    fun makeCourse() {
+        makePPT()
+        makeVideo()
+        if (needWriteAtricle()) {
+            makeArticle()
+        }
+        packageCourse()
+    }
+
+    fun makePPT() {
+        println("make ppt")
+    }
+    fun makeVideo() {
+        println("make video")
+    }
+
+    fun makeArticle() {
+        println("make article")
+    }
+
+    open fun needWriteAtricle():Boolean {
+        return false
+    }
+
+    abstract fun packageCourse()
+}
+
+class LinuxCourse : ACourse() {
+    override fun packageCourse() {
+        println("package linux")
+    }
+}
+
+class JavaCourse : ACourse() {
+    override fun needWriteAtricle(): Boolean {
+        return true
+    }
+
+    override fun packageCourse() {
+        println("package java")
+    }
+}
+
+fun main() {
+    val linux: ACourse = LinuxCourse()
+    linux.makeCourse()
+    println("-------------")
+    val java: ACourse = JavaCourse()
+    java.makeCourse()
+}
